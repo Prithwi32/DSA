c9ecb3ea77f533599d1cd30076ee8f60a58f7abf
Prithwi <prithwi.online11@gmail.com>
2023-11-02 19:55:01 +0530

    subArray, 2sum problems Added

---
+++ java-AtoZ DSA/Arrays/longestSubArray.java
@@ -0,0 +1,69 @@
+package Arrays;
+
+import java.util.*;
+
+public class longestSubArray {
+
+/*
+static int findSubArrayLength(int[] arr, int n, int target) {
+        int left = 0;
+        int right = 0;
+        long sum = arr[0];
+        int maxLen = 0;
+        while (right < n) {
+            while (left <= right && sum > target) {
+                sum -= arr[left];
+                left++;
+            }
+            if (sum == target) {
+                maxLen = Math.max(right - left + 1, maxLen);
+            }
+            right++;
+            if (right < n) {
+                sum += arr[right];
+            }
+        }
+        return maxLen;
+    }
+*/
+
+static int findUsingHashMap(int[] arr, int n, int target) {
+        Map<Integer, Integer> mpp = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum == target) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            int rem = sum - target;
+            if (mpp.containsKey(rem)) {
+                int len = i - mpp.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if (!mpp.containsKey(sum)) {
+                mpp.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        // int len = findSubArrayLength(arr, n, target);
+         int len = findUsingHashMap(arr, n, target);
+        System.out.println(len);
+        sc.close();
+    }
+}
---
+++ java-AtoZ DSA/Arrays/twoSum.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.*;
+
+public class twoSum {
+
+    static String check(int[] arr, int n, int target) {
+        Arrays.sort(arr);
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return "Yes";
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return "No";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        String ans = check(arr, n, target);
+        System.out.println(ans);
+        sc.close();
+    }
+}
