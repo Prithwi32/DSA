6e9648b6e8c2e17b0afccf6c2c6f0b92660e5d50
Prithwi <prithwi.online11@gmail.com>
2023-11-12 20:47:12 +0530

    4sum problem Added

---
+++ java-AtoZ DSA/Arrays/Hard/fourSum.java
@@ -0,0 +1,62 @@
+package Arrays.Hard;
+
+import java.util.*;
+
+public class fourSum {
+    public static List<List<Integer>> findFourSum(int arr[], int n, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j > i+1 && arr[j] == arr[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = arr[i];
+                    sum += arr[j];
+                    sum += arr[k];
+                    sum += arr[l];
+
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
+                        temp.add(arr[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(k<l && arr[l]==arr[l+1]) l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        List<List<Integer>> ans = findFourSum(arr, n, target);
+        for (List<Integer> it : ans) {
+            for (int ele : it) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
