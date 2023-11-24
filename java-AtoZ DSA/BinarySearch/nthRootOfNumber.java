29d64883592e82960bc1f48412e8e138c863806c
Prithwi <prithwi.online11@gmail.com>
2023-11-24 23:02:50 +0530

    bs for answers

---
+++ java-AtoZ DSA/BinarySearch/nthRootOfNumber.java
@@ -0,0 +1,43 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class nthRootOfNumber {
+    public static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m)
+                return 2;
+        }
+        if (ans == m)
+            return 1;
+        return 0;
+    }
+
+    public static int findNthRoot(int n, int m) {
+        int low = 1;
+        int high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int ans = findNthRoot(n, m);
+        System.out.println(ans);
+        sc.close();
+    }
+}
---
+++ java-AtoZ DSA/BinarySearch/sqrtOfInteger.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class sqrtOfInteger {
+    public static int findSqrt(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long val = mid * mid;
+            if (val > (long)n) {
+                high = (int)mid - 1;
+            } else {
+                low = (int)mid + 1;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = findSqrt(n);
+        System.out.println(ans);
+        sc.close();
+    }
+}
