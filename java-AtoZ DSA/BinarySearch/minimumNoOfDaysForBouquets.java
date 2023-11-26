741d7d9abfd01caa32ed0e22c8ca4f07a6f1c005
Prithwi <prithwi.online11@gmail.com>
2023-11-26 23:52:15 +0530

    smallest divisor

---
+++ java-AtoZ DSA/BinarySearch/minimumNoOfDaysForBouquets.java
@@ -0,0 +1,84 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class minimumNoOfDaysForBouquets {
+    public static boolean possible(int arr[], int day, int m, int k) {
+        int n = arr.length;
+        int noOfB = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            } else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+
+    public static int findBrute(int arr[], int m, int k) {
+        int n = arr.length;
+        long val = (long) m * k;
+        if (val > n)
+            return -1;
+        int mini = Integer.MAX_VALUE;
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+        for (int i = mini; i <= maxi; i++) {
+            if (possible(arr, i, m, k)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int findBs(int arr[], int m, int k) {
+        int n = arr.length;
+        long val = (long) m * k;
+        if (val > n) {
+            return -1;
+        }
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+        int low = mini;
+        int high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int m = sc.nextInt(); // no. of bouquets
+        int k = sc.nextInt(); // adjacent flowers required.
+        // int ans = findBrute(arr, m, k);
+        int ans = findBs(arr, m, k);
+        if (ans == -1) {
+            System.out.println("We cannot make bouquets.");
+        } else {
+            System.out.println("We can make bouquets on day " + ans);
+        }
+
+        sc.close();
+    }
+}
---
+++ java-AtoZ DSA/BinarySearch/smallestDivisor.java
@@ -0,0 +1,46 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class smallestDivisor {
+    public static int sumByD(int arr[], int div) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double) (arr[i]) / (double) (div));
+        }
+        return sum;
+    }
+
+    public static int find(int arr[], int limit) {
+        int n = arr.length;
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+        int low = 1;
+        int high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int limit = sc.nextInt();
+        int ans = find(arr, limit);
+        System.out.println("The minimum divisor is: " + ans);
+        sc.close();
+    }
+}
