fa2e1317fea9ff26c62fec91696a145ecb66ea02
Prithwi <prithwi.online11@gmail.com>
2024-01-13 17:53:34 +0530

    sortll

--- java-AtoZ DSA/LinkedList/SinglyLL/sortll.java
+++ java-AtoZ DSA/LinkedList/SinglyLL/sortll.java
@@ -18,19 +18,68 @@ public class sortll {
 
     // brute force approach
     public static Node sortLinkedList(Node head) {
-        Node temp = head;
-        ArrayList<Integer> l = new ArrayList<>();
-        while (temp != null) {
-            l.add(temp.data);
-            temp = temp.next;
+    Node temp = head;
+    ArrayList<Integer> l = new ArrayList<>();
+    while (temp != null) {
+    l.add(temp.data);
+    temp = temp.next;
+    }
+    temp = head;
+    Collections.sort(l);
+    for (int i = 0; i < l.size(); i++) {
+    temp.data = l.get(i);
+    temp = temp.next;
+    }
+    return head;
+    }
+
+    // Using Merge Sort
+
+    public static Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        Collections.sort(l);
-        for (int i = 0; i < l.size(); i++) {
-            temp.data = l.get(i);
-            temp = temp.next;
+        return slow;
+    }
+
+    public static Node merge(Node list1, Node list2) {
+        Node ans = new Node(-1);
+        Node temp = ans;
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
         }
-        return head;
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+        return ans.next;
+    }
+
+    public static Node sortLL(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node middle = findMiddle(head);
+        Node left = head;
+        Node right = middle.next;
+        middle.next = null;
+
+        Node leftsort = sortLL(left);
+        Node rightsort = sortLL(right);
+
+        return merge(leftsort, rightsort);
     }
 
     // Utility function to print the linked list
@@ -54,7 +103,8 @@ public class sortll {
         printLinkedList(head);
 
         // Applying the brute force sorting approach
-        head = sortLinkedList(head);
+        // head = sortLinkedList(head);
+        head = sortLL(head);
 
         System.out.println("Sorted Linked List:");
         printLinkedList(head);
